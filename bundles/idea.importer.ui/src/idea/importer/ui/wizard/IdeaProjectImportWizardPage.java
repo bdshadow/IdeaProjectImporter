9ca37b1f3757190ac409099ea41ca064e707bb0b
bdshadow <bdshadow@gmail.com>
2015-11-30 17:58:37 +0300

    Wizard page ui elements added

--- bundles/idea.importer.ui/src/idea/importer/ui/wizard/IdeaProjectImportWizardPage.java
+++ bundles/idea.importer.ui/src/idea/importer/ui/wizard/IdeaProjectImportWizardPage.java
@@ -1,10 +1,21 @@
 package idea.importer.ui.wizard;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Label;
 
 public class IdeaProjectImportWizardPage extends WizardPage {
+	
+	protected Combo rootDirectoryCombo;
 
 	protected IdeaProjectImportWizardPage() {
 		super("Intellij Idea Projects");
@@ -14,8 +25,40 @@ public class IdeaProjectImportWizardPage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
-		Composite  container = new Composite(parent, SWT.NONE);
-		setControl(container);
+	    Composite composite = new Composite(parent, SWT.NONE);
+	    composite.setLayout(new GridLayout(3, false));
+	    setControl(composite);
+
+	      final Label selectRootDirectoryLabel = new Label(composite, SWT.NONE);
+	      selectRootDirectoryLabel.setLayoutData(new GridData());
+	      selectRootDirectoryLabel.setText("Location:");
+
+	      rootDirectoryCombo = new Combo(composite, SWT.NONE);
+	      rootDirectoryCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+	      rootDirectoryCombo.setFocus();
+
+	      final Button browseButton = new Button(composite, SWT.NONE);
+	      browseButton.setText("Browse...");
+	      browseButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+	      browseButton.addSelectionListener(new SelectionAdapter() {
+	        public void widgetSelected(SelectionEvent e) {
+	          DirectoryDialog dialog = new DirectoryDialog(getShell(), SWT.NONE);
+	          dialog.setText("Select folder:");
+	          String path = rootDirectoryCombo.getText();
+	          if(path.length() == 0) {
+	            path = ResourcesPlugin.getWorkspace().getRoot().getLocation().toPortableString();
+	          }
+	          dialog.setFilterPath(path);
+
+	          String result = dialog.open();
+	          if(result != null) {
+	            rootDirectoryCombo.setText(result);
+//	            if(rootDirectoryChanged()) {
+//	              scanProjects();
+//	            }
+	          }
+	        }
+	      });
 	}
 
 }
